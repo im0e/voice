a135cc55cba432183bdd9194ec274f0160d3913d
im0e <moe.github.website@gmail.com>
2022-05-14 19:05:10 +0300

    small corrections or destructions

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
+
+/*
+
+
+THE PREVIOUS TEST 
+
+
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var recognition = new SpeechRecognition();
 var output;
@@ -25,4 +32,43 @@ recognition.onresult = function(event) {
 recognition.onspeechend = function() {
   recognition.stop();
 }
+*/
 
+
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+var output;
+
+var counter = 0;
+var interval = setInterval( function() {counter++;}, 1000);
+
+
+var record_notification = new Audio("res/sounds/start.wav");
+
+function read(id) {
+  txt = document.getElementById(id).innerHTML;
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(txt));
+}
+
+function start_record(id) {
+  record_notification.play();
+  record_notification.currentTime = 0;
+  output = document.getElementById(id);
+  recognition.start();
+}
+
+recognition.onresult = function (event) {
+  var txt = event.results[0][0].transcript;
+  console.log(txt);
+  output.value += txt;
+};
+
+recognition.onend = function(){
+  recognition.start();
+}
+
+
+recognition.onspeechend = function () {
+  recognition.stop();
+};
